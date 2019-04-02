9d2b534bacc034c6c62cf6b8871de08b5e1ae9c0
Michael Evans <mmevansjr@gmail.com>
2019-04-02 12:40:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,8 @@ Both functions take two parameters, an array and an element to add to the beginn
 the element to the beginning of the array and then return the whole array. The destructive function, 
 `destructivelyAddElementToBeginningOfArray`, should alter the original array that's passed in; `addElementToBeginningOfArray`, 
 on the other hand, should return a new array **and not modify the original**.
-==Task 2: In `arrays.js`, define two functions, `addElementToBeginningOfArray` and `destructivelyAddElementToBeginningOfArray`. Both functions take two parameters, an array and an element to add to the beginning of the array, and both functions should add the element to the beginning of the array and then return the whole array. The destructive function, `destructivelyAddElementToBeginningOfArray`, should alter the original array that's passed in; `addElementToBeginningOfArray`, on the other hand, should return a new array **and not modify the original**.
+=======
+Task 2: In `arrays.js`, define two functions, `addElementToBeginningOfArray` and `destructivelyAddElementToBeginningOfArray`. Both functions take two parameters, an array and an element to add to the beginning of the array, and both functions should add the element to the beginning of the array and then return the whole array. The destructive function, `destructivelyAddElementToBeginningOfArray`, should alter the original array that's passed in; `addElementToBeginningOfArray`, on the other hand, should return a new array **and not modify the original**.
 
 */
 function destructivelyAddElementToBeginningOfArray(cat, dog) {
